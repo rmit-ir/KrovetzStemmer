9448182c14d92e38c8df85948ef2f1953c436053
Ruey-Cheng Chen <rueycheng@gmail.com>
2017-04-14 00:45:07 +1000

    Add python 3 support

--- krovetzstemmer_module.cpp
+++ krovetzstemmer_module.cpp
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include <bytesobject.h>
 
 #include <iostream>
 
@@ -30,7 +31,8 @@ Stemmer_dealloc(Stemmer* self)
     delete self->stemmer;
     self->stemmer = NULL;
 
-    self->ob_type->tp_free((PyObject*)self);
+    /* self->ob_type->tp_free((PyObject*)self); */
+    Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
 static PyObject*
@@ -43,26 +45,27 @@ Stemmer_stem(Stemmer* self, PyObject* args) {
     PyObject* stem;
     if (is_unicode) 
 	stem = PyUnicode_AsUTF8String(term); 
-    else if (PyString_Check(term))
-	stem = PyString_FromString(PyString_AS_STRING(term));
+    else if (PyBytes_Check(term))
+	stem = PyBytes_FromString(PyBytes_AS_STRING(term));
     else {
 	PyErr_BadArgument();
 	return NULL;
     }
 
     int processed = self->stemmer->kstem_stem_tobuffer(
-	PyString_AS_STRING(stem), self->buf);
+	PyBytes_AS_STRING(stem), self->buf);
+
     if (processed > 0) {
 	Py_DECREF(stem);
 
 	if (is_unicode) 
 	    return PyUnicode_DecodeUTF8(self->buf, processed - 1, NULL);
 	else
-	    return PyString_FromStringAndSize(self->buf, processed - 1);
+	    return PyBytes_FromStringAndSize(self->buf, processed - 1);
     }
     else {
 	if (is_unicode) {
-	    PyObject* unicode = PyUnicode_FromString(PyString_AS_STRING(stem));
+	    PyObject* unicode = PyUnicode_FromString(PyBytes_AS_STRING(stem));
 	    Py_DECREF(stem);
 	    return unicode;
 	}
@@ -77,16 +80,13 @@ Stemmer_call(Stemmer* self, PyObject* args, PyObject* kwargs) {
 }
 
 static PyMethodDef Stemmer_methods[] = {
-    {"stem", (PyCFunction)Stemmer_stem, METH_VARARGS,
-	"Return the stem of the given term"},
-
-    {NULL}    /* Sentinel */
+    {"stem", (PyCFunction)Stemmer_stem, METH_VARARGS, "Return the stem of the given term"},
+    {NULL, NULL}    /* Sentinel */
 };
 
 static PyTypeObject StemmerType = {
-    PyObject_HEAD_INIT(NULL)
+    PyVarObject_HEAD_INIT(NULL, 0)
 
-    0,                           /* ob_size */
     "krovetzstemmer.Stemmer",    /* tp_name */
     sizeof(Stemmer),             /* tp_basicsize */
     0,                           /* tp_itemsize */
@@ -107,7 +107,7 @@ static PyTypeObject StemmerType = {
     0,                           /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,          /* tp_flags */
     "KrovetzStemmer objects",    /* tp_doc */
-     0,		                 /* tp_traverse */
+    0,		                 /* tp_traverse */
     0,		                 /* tp_clear */
     0,		                 /* tp_richcompare */
     0,		                 /* tp_weaklistoffset */
@@ -130,18 +130,39 @@ static PyMethodDef krovetzstemmer_methods[] = {
     {NULL}    /* Sentinel */
 };
 
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,  /* m_base */
+    "krovetzstemmer",       /* m_name */
+    NULL,                   /* m_doc */
+    -1,                     /* m_size */
+    krovetzstemmer_methods  /* m_methods */
+};
+#endif
+
 #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
 #define PyMODINIT_FUNC void
 #endif
 PyMODINIT_FUNC
+#if PY_MAJOR_VERSION >= 3
+PyInit_krovetzstemmer(void)
+{
+    if (PyType_Ready(&StemmerType) < 0) return NULL;
+    PyObject* m = PyModule_Create(&moduledef);
+    if (m == NULL) return NULL;
+#else
 initkrovetzstemmer(void) 
 {
     if (PyType_Ready(&StemmerType) < 0) return;
-
     PyObject* m = Py_InitModule3("krovetzstemmer", krovetzstemmer_methods, 
 				 "Python binding to the KrovetzStemmer package 4.3 (C++ version)");
     if (m == NULL) return;
+#endif
 
     Py_INCREF(&StemmerType);
     PyModule_AddObject(m, "Stemmer", (PyObject*)&StemmerType);
+
+#if PY_MAJOR_VERSION >= 3
+    return m;
+#endif
 }
