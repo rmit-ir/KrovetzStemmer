6498de94784405c9ca77c6515a59f0bb99377c08
Ruey-Cheng Chen <rueycheng@gmail.com>
2017-12-22 23:33:45 +1100

    Fixed the issue with stdlib on macOS 10.12+

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ for key, value in cfg_vars.items():
 
 setup(
     name="KrovetzStemmer",
-    version="0.5",
+    version="0.6",
     description="Python binding to the C++ KrovetzStemmer package",
     author="Ruey-Cheng Chen",
     author_email="rueycheng@gmail.com",
@@ -16,8 +16,8 @@ setup(
     ext_modules=[
         Extension(
             "krovetzstemmer",
-            sources=["krovetzstemmer_module.cpp",
-                     "KrovetzStemmer.cpp"]
+            extra_compile_args=['-stdlib=libstdc++'],
+            sources=["krovetzstemmer_module.cpp", "KrovetzStemmer.cpp"]
         )
     ]
 )
