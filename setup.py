0d8b42534cf2f2fb52f20063009a84a0481e39f1
Ruey-Cheng Chen <rcchen@seek.com.au>
2020-11-08 17:01:09 +1100

    Update setup.py so project info is visible on PyPI

--- setup.py
+++ setup.py
@@ -4,19 +4,28 @@ from distutils.core import setup, Extension
 cfg_vars = get_config_vars()
 for key, value in cfg_vars.items():
     if type(value) == str:
-        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+        cfg_vars[key] = value.replace('-Wstrict-prototypes', '')
+
+long_description = open('README.md').read()
 
 setup(
-    name="KrovetzStemmer",
-    version="0.6",
-    description="Python binding to the C++ KrovetzStemmer package",
-    author="Ruey-Cheng Chen",
-    author_email="rueycheng@gmail.com",
-    url="https://github.com/rmit-ir/KrovetzStemmer",
+    name='KrovetzStemmer',
+    version='0.7',
+    description='Python binding to the C++ KrovetzStemmer package',
+    long_description=long_description,
+    author='Ruey-Cheng Chen',
+    author_email='rueycheng@gmail.com',
+    url='https://github.com/rmit-ir/KrovetzStemmer',
     ext_modules=[
         Extension(
-            "krovetzstemmer",
-            sources=["krovetzstemmer_module.cpp", "KrovetzStemmer.cpp"]
+            'krovetzstemmer',
+            sources=['krovetzstemmer_module.cpp', 'KrovetzStemmer.cpp']
         )
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing'
+    ],
+    license='MIT'
 )
